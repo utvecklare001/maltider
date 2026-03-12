893efcb10438d5794ac03aa2f74c6d5f3aafd999
Anders L <99913092+utvecklare001@users.noreply.github.com>
2026-03-12 11:16:19 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "./"
+  base: '/maltider/'
 })
 export default defineConfig({
   plugins: [react()],
