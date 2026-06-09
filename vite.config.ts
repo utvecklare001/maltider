0f068fa7b23d1d580123bd8bdfe131e3e82e273a
Anders L <99913092+utvecklare001@users.noreply.github.com>
2026-06-09 16:51:29 +0200

    Add base path to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/maltider/', // <-- LÄGG TILL DENNA RAD!
+  base: '/maltider/', // <-- Lägg till denna rad!
 })
