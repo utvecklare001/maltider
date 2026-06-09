cb726284195f475bc69892a0d0e9b0b15484d2e2
Anders L <99913092+utvecklare001@users.noreply.github.com>
2026-06-09 16:49:14 +0200

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/maltider/",
-  plugins: [react()]
+  plugins: [react()],
+  base: '/maltider/', // <-- LÄGG TILL DENNA RAD!
 })
